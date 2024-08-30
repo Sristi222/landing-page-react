fd46e4bd918a32a33ed99b0c8c319d1a1d9f4fb6
Sristi Shrestha <sristishrestha80@gmail.com>
2024-08-30 22:22:49 +0545

    Login changed

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/Features" element={<Features />} />
-        <Route path="/login" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
